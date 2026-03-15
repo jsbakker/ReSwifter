f6152083f8455eaf7db8fbbe1fe29bfdb418877c
Jeffrey Bakker <jsbdev@outlook.com>
2026-03-14 18:20:34 -0700

    Add credits in About screen.

--- ReSwifter/ReSwifterApp.swift
+++ ReSwifter/ReSwifterApp.swift
@@ -34,6 +34,49 @@ struct ReSwifterApp: App {
                 SnippetCommandMenu(viewModel: viewModel)
                     .modelContainer(modelContainer)
             }
+
+            CommandGroup(replacing: .appInfo) {
+                Button("About ReSwifter") {
+                    NSApplication.shared.orderFrontStandardAboutPanel(
+                        options: [
+                            .credits: NSAttributedString(
+                                string: """
+                                ReSwifter Copyright (c) 2026 Jeffrey Bakker
+
+                                Syntax highlighting provided by CodeEditor framework, Copyright (c) 2021 ZeeZide, MIT License:
+                                https://github.com/ZeeZide/CodeEditor
+
+                                MIT License
+
+                                Permission is hereby granted, free of charge, to any person obtaining a copy
+                                of this software and associated documentation files (the "Software"), to deal
+                                in the Software without restriction, including without limitation the rights
+                                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                                copies of the Software, and to permit persons to whom the Software is
+                                furnished to do so, subject to the following conditions:
+
+                                The above copyright notice and this permission notice shall be included in all
+                                copies or substantial portions of the Software.
+
+                                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                                SOFTWARE.
+                                """,
+                                attributes: [
+                                    .font: NSFont.systemFont(ofSize: 11),
+                                    .foregroundColor: NSColor.textColor
+                                ]
+                            ),
+                            .applicationName: "ReSwifter",
+                            .applicationVersion: "26.03",
+                        ]
+                    )
+                }
+            }
         }
     }
 }
